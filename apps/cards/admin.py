9b94b89bb8ccee19c5a185a59c29d82e72251cf6
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-03-05 11:40:50 +0300

    initial commit

---
+++ apps/cards/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Card
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('deck', 'question', 'bucket' )
+
+
+admin.site.register(Card, CardAdmin)
---
+++ apps/decks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Deck(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    last_reviewed = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
